700cca3709ee1c6ec5fc4a8859c5a3bf4bb3d34e
Dandelion <1169809321@qq.com>
2026-05-28 11:21:07 +0800

    ltp goal upd

--- user/app/initcode-rv.cc
+++ user/app/initcode-rv.cc
@@ -5,19 +5,19 @@ extern "C"
     __attribute__((section(".text.startup"))) int main()
     {
         init_env("/musl/");
-        basic_test("/musl/");
-        basic_test("/glibc/");
-        iozone_test("/musl");
-        iozone_test("/glibc");
-        libc_test("/musl/");
-        lua_test("/musl/");
-        lua_test("/glibc/");
-        libcbench_test("/musl");
-        libcbench_test("/glibc");
         ltp_test(true);
-        ltp_test(false);
-        busybox_test("/musl/");
-        busybox_test("/glibc/");
+        // basic_test("/musl/");
+        // basic_test("/glibc/");
+        // iozone_test("/musl");
+        // iozone_test("/glibc");
+        // libc_test("/musl/");
+        // lua_test("/musl/");
+        // lua_test("/glibc/");
+        // libcbench_test("/musl");
+        // libcbench_test("/glibc");
+        // ltp_test(false);
+        // busybox_test("/musl/");
+        // busybox_test("/glibc/");
         shutdown();
         return 0;
     }
