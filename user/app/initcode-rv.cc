29d312c8a8f34626032af94fb3cfffe71b9b0d10
Dandelion <1169809321@qq.com>
2026-05-29 08:48:04 +0800

    添加测例

--- user/app/initcode-rv.cc
+++ user/app/initcode-rv.cc
@@ -5,23 +5,23 @@ extern "C"
     __attribute__((section(".text.startup"))) int main()
     {
         init_env("/musl/");
-        // netperf_test("/musl/");
-        // netperf_test("/glibc/");
-        // iperf_test("/musl/");
-        // iperf_test("/glibc/");
-        // iozone_test("/musl");
-        // iozone_test("/glibc");
-        // libc_test("/musl/");
-        // basic_test("/musl/");
-        // basic_test("/glibc/");
-        // lua_test("/musl/");
-        // lua_test("/glibc/");
-        // libcbench_test("/musl");
-        // libcbench_test("/glibc");
+        netperf_test("/musl/");
+        netperf_test("/glibc/");
+        iperf_test("/musl/");
+        iperf_test("/glibc/");
+        iozone_test("/musl");
+        iozone_test("/glibc");
+        libc_test("/musl/");
+        basic_test("/musl/");
+        basic_test("/glibc/");
+        lua_test("/musl/");
+        lua_test("/glibc/");
+        libcbench_test("/musl");
+        libcbench_test("/glibc");
         ltp_test(true);
         ltp_test(false);
-        // busybox_test("/musl/");
-        // busybox_test("/glibc/");
+        busybox_test("/musl/");
+        busybox_test("/glibc/");
         shutdown();
         return 0;
     }
